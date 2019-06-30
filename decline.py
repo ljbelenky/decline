9ad4da17932a3f6576adec3cab7fbb237edf1ba3
ljbelenky <ljbelenky@gmail.com>
2019-06-30 11:38:39 -0600

    building 1D convolution neural network

--- decline.py
+++ decline.py
@@ -1,82 +1,91 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression as LR
+import random
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Conv1D, Dense
+
+
 
 class Decline_Generator():
     '''This class creates synthetic gas production data with dropouts, low point, high points and noise.
     Return data is a pandas dataframe with columns "date" and "production'''
-    def __init__(self, n = 366, drops = (0,0), 
-    noise = 0, highpoints = (0,0), lowpoints = (0,0), decline_rate = .996):
-        dates = pd.date_range(start = '2018-01-01', end = '2019-01-01', periods = n)
-        x = 100
+    def __init__(self, initial = 100, drops = {'num':0,'max_length':0}, noise = 0, highpoints = (0,0), lowpoints = (0,0), decline_rate = .996, flush = 8):
+        n = 256
+        # dates = pd.date_range(start = '2018-01-01', periods = n)
+        production = initial
         base_production = []
         for _ in range(n):
-            base_production.append(x)
-            x*=decline_rate
-        data = pd.DataFrame({'date':dates,'production':base_production})
+            base_production.append(production)
+            production*=decline_rate
+        data = pd.DataFrame({'production':base_production})
 
-        data.production += np.random.random(size = n)*noise
+        self.target = production
+
+        data.production += np.random.random(size = n)*noise*initial/100
 
         for i in range(highpoints[0]):
             index = np.random.randint(n)
-            data.production.iat[index] += highpoints[1]
+            data.production.iat[index] += highpoints[1]*initial/100
 
         for i in range(lowpoints[0]):
             index = np.random.randint(n)
-            data.production.iat[index] -= lowpoints[1]
+            data.production.iat[index] -= lowpoints[1]*initial/100
 
-        for i in range(drops[0]):
-            start = np.random.randint(n-drops[1])
-            data.production.iloc[start:start+drops[1]] = 0
+        for i in range(drops['num']):
+            start = np.random.randint(n)
+            length = np.random.randint(drops['max_length'])
+            data.production.iloc[start:start+length] = 0  
             
-        data.production += np.random.random(size = n)*noise
-        self.data = data
+        data.production += np.random.random(size = n)*noise-noise/2
+        data.production = data.production.map(lambda x:max(x,0))
+        self.data = data.values.flatten()
+
+    def __call__(self):
+        return self.data, self.target
 
 if __name__ == '__main__':
-    data = Decline_Generator(drops = (10,15), noise = 8, highpoints = (40,20), lowpoints = (10,20)).data
 
-    X = data.copy()
-    X['production'] = np.log(X['production'])
+    Xs, ys = [], []
 
-    '''Add Delta Columns for differences''' 
-    for i in range(-4,5):
-        X['delta{}'.format(i)] = X.production.diff(i)
+    for i in range(10):
+        initial = np.random.uniform(1000)
+        decline_rate = np.random.uniform(.995, .9999)
+        drops = {'num':np.random.randint(15), 'max_length':np.random.randint(1,20)}
+        noise = np.random.uniform(16)
+        highpoints = np.random.randint(20), np.random.uniform(50)
+        lowpoints = np.random.randint(20), np.random.uniform(40)
 
-    X['days_ago'] = (X['date'].max()-X['date']).dt.days
-    X['epsilon'] = X['days_ago']/X['production']
+        dg = Decline_Generator(initial = initial, drops = drops, noise = noise, highpoints = highpoints, lowpoints = lowpoints, decline_rate = decline_rate)
+        X,y = dg()
+        Xs.append(X)
+        ys.append(y)
 
-    X = X.drop(['date','days_ago'], axis = 1).reset_index()
-    X = X.dropna()
-    indices = X['index']
-    X.drop('index',axis = 1, inplace = True)
+    data = pd.DataFrame(Xs)
+    data['target'] = ys
 
-    X = PCA(n_components = int(len(X.columns)/1.5), whiten = True).fit_transform(X)
-    labels = KMeans(n_clusters = 5).fit(X).predict(X)
-    data['labels'] = -1
-    data['labels'].iloc[indices] = labels
-    data = data[data['labels']>=0]
-
-    majority_class = data.groupby('labels').count().sort_values('date').index[-1]
-    majority = data[data['labels']==majority_class].drop('labels', axis = 1)
-    majority['log_production'] = np.log(majority['production'])
-    
-    model = LR().fit(majority['date'].values.reshape(-1,1), majority['log_production'])
-    first_pass_predictions = model.predict(majority['date'].values.astype(float).reshape(-1,1))
-    first_pass_residuals = first_pass_predictions - majority['log_production']
-
-    mean, std = first_pass_residuals.mean(), first_pass_residuals.std()    
-    filter = (first_pass_residuals > mean-2*std)&(first_pass_residuals<mean+2*std)
-
-    filtered_majority = majority[filter]
-    filtered_model = LR().fit(filtered_majority['date'].values.reshape(-1,1), filtered_majority['log_production'])
-    prediction_dates = pd.date_range(start = majority.date.min(), end = majority.date.max() + pd.DateOffset(days = 100))
-    predictions = filtered_model.predict(prediction_dates.values.astype(float).reshape(-1,1))
-    predictions = np.exp(predictions)
-
-    plt.plot(prediction_dates, predictions, color = 'green')
-    plt.plot(data.set_index('date')['production'], alpha = .2)
-    plt.scatter(data['date'], data['production'])
+    data.transpose().plot()
     plt.show()
+
+    X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1])
+
+    model = LR().fit(X_train, y_train)
+    print(model.score(X_train, y_train))
+    print(model.score(X_test, y_test))
+
+    # plt.scatter(y_train, model.predict(X_train))
+    # plt.scatter(y_test, model.predict(X_test))
+    # plt.show()
+
+    model_m = Sequential()
+    model_m.add(Conv1D(100, 10, activation='relu', input_shape=(None, X_train.shape[1])))
+    model_m.add(Conv1D(100, 10, activation='relu'))
+    model_m.add(Conv1D(160, 10, activation='relu'))
+    model_m.add(Conv1D(160, 10, activation='relu'))
+    model_m.add(Dense(1, activation='linear'))
+    print(model_m.summary())
+    model_m.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    model_m.fit(X_train,y_train)
+    
